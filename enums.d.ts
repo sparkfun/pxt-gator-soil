f4e62e70688b8796933d0bf49f17743bcacd319e
Andrew England <andrew.england@sparkfun.local>
2019-04-17 16:07:07 -0600

    ran pxt build, now committing

--- enums.d.ts
+++ enums.d.ts
@@ -1,5 +1,5 @@
 // Auto-generated. Do not edit.
-declare namespace gatorlight {
+declare namespace gatorMoisture {
 }
 
 // Auto-generated. Do not edit. Really.
--- shims.d.ts
+++ shims.d.ts
@@ -16,7 +16,7 @@
      * Distributed as-is; no warranty is given.
      */
 
-declare namespace gatorlight {
+declare namespace gatorMoisture {
 }
 
 // Auto-generated. Do not edit. Really.
